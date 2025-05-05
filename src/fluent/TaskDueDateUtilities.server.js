a1d4e7a6305a9ec74d2f2c6cff02836684c8e502
System Administrator <admin@example.com>
2025-05-04 22:26:50 -0700

    Add script include and types

---
+++ src/fluent/TaskDueDateUtilities.server.js
@@ -0,0 +1,14 @@
+const TaskDueDateUtilities = Class.create();
+TaskDueDateUtilities.prototype = Object.extendsObject(global.AbstractAjaxProcessor, {
+  type: "TaskDueDateUtilities",
+
+  addDays: (sysId, numberOfDays) => {
+    const record = new GlideRecord('sn_todo_task');
+    if(!record.get(sysId)){
+      throw new Error(`${sysId} was not found`)
+    }
+
+    const { addDaysToDueOn } = require('src/server/date-utilities.ts')
+    addDaysToDueOn(record, numberOfDays)
+  }
+})
--- src/server/date-utilities.ts
+++ src/server/date-utilities.ts
@@ -1,5 +1,5 @@
 import { gs, GlideRecord, GlideDateTime } from "@servicenow/glide"
-import { format, addDays } from "date-fns"
+import { format, addDays, fromUnixTime } from "date-fns"
 
 export function checkRecordSateUpdateClosedOn(current: GlideRecord): void {
     gs.info(`updating closed_on for record: ${current.getLink()}`);
@@ -8,9 +8,17 @@ export function checkRecordSateUpdateClosedOn(current: GlideRecord): void {
 }
 
 export function autoSetDueOn(current: GlideRecord): void {
-    gs.info('setting due on date');
+    gs.info(`setting due on date for ${current.getValue('sys_id')}`);
 
-    const dueAt = addDays(new Date(), 1);
+    addDaysToDueOn(current, 1)
+}
+
+export function addDaysToDueOn(current: GlideRecord, daysToAdd: number = 1): void {
+    const currentDueOn = current.getValue("due_on")
+    
+    const jsDateDueAt = currentDueOn ? fromUnixTime(new GlideDateTime(currentDueOn).getNumericValue()) : new Date();
+
+    const dueAt = addDays(jsDateDueAt, daysToAdd);
 
     current.setValue("due_on", formatSN(dueAt))
 }
